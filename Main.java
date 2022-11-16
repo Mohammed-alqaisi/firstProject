6698fcc7e6713a3ebc30b97f6bdbe899c3a7c4a3
mohammed-alqaisi <mqaisi612@gmail.com>
2022-11-16 12:38:04 +0300

    second commit on test branch

--- Main.java
+++ Main.java
@@ -18,5 +18,6 @@ class Main {
 
         System.out.println("Reversed Number: " + reversed);
         System.out.println("Hello world!");
+        System.out.println("this edit is on the test branch");
     }
 }
